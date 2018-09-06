058e61c1568dbc7c85d818d87681cf71f6e432b6
Sabu <sabuw319@gmail.com>
2018-09-06 20:36:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,29 @@ function destructivelyAppendKitten(name){
   kittens.push(name)
 }
 
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destuctivelyRemovelastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  kittens = [...kittens, name]
+  return kittens
+}
+
+function prependKitten(name){
+  kittens = [name, ...kittens]
+  return kittens
+}
+
+function removelastkitten(){
+  kittens.slice(0, kittens.length - 1)
+  return kittens
+}
